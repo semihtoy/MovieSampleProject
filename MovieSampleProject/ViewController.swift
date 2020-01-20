221aad0d567839d96eb6f915145e1f514ee99a20
ssemih42 <ssemih4238@gmail.com>
2020-01-20 21:20:19 +0300

    Create Project

---
+++ MovieSampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieSampleProject
+//
+//  Created by Xcode on 20.01.2020.
+//  Copyright © 2020 Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
